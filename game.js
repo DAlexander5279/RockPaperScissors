5baf0ce1c2063781b1893f6385aeaf4e5ae54ed8
DAlexander5279 <dalton.alexander@hotmail.com>
2023-10-13 16:49:40 -0400

    play rounds by comparing player&computer choice

--- game.js
+++ game.js
@@ -2,7 +2,93 @@
 
 
 function getComputerChoice(){
-    answers = ["Rock", "Paper", "Scissors"]
-    index = Math.floor(Math.random() * 3)
+    const answers = ["Rock", "Paper", "Scissors"]
+    let index = Math.floor(Math.random() * 3)
     return answers[index]
+    
 }
+
+
+function playRound(playerSelection, computerSelection){
+    let p = String(playerSelection).toLowerCase()
+    let c = String(computerSelection).toLowerCase()
+
+
+    switch(p){
+
+
+
+
+        case "rock":
+            switch(c){
+                case "rock":
+                        return "Draw!"
+                    break;
+
+                case "paper":
+                        return "Paper beats rock, you lose!"
+                    break;
+
+                case "scissors":
+                        return "rock beats scissors, you win!"
+                    break;
+            }
+
+            break;
+
+
+
+
+
+
+        case "paper":
+            switch(computerSelection){
+                case "rock":
+                        return "Paper beats rock, you win!"
+                    break;
+
+                case "paper":
+                    return "Draw!"
+                    break;
+
+                case "scissors":
+                    return "scissors beats paper, you lose!"
+                    break;
+            }
+
+            break;
+
+
+
+
+
+
+
+        case "scissors":
+            switch(computerSelection){
+                case "rock":
+                    return "rock beats scissors, you lose!"
+                    break;
+
+                case "paper":
+                    return "scissors beats paper, you lose!"
+                    break;
+
+                case "scissors":
+                    return "Draw!"
+                    break;
+            }
+            break;
+
+
+
+
+    }
+
+
+    
+    
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
